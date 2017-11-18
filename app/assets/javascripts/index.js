60da96202588d9f513c6a1eeca9edf7b5ba0d786
Sergiiss <Dracula23@mail.ru>
2017-11-18 21:58:17 +0300

    Add a change of text with 'Show' on 'Hide'

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -3,8 +3,10 @@ function showArticles(event) {
 
   if (document.getElementById('toggle').style.display == ''){
     document.getElementById('toggle').style.display = 'block';
+    document.getElementById('show-hide-text').innerHTML = 'Hide planned articles';
   }
   else{
     document.getElementById('toggle').style.display = '';
+    document.getElementById('show-hide-text').innerHTML = 'Show planned articles';
   }
 };
