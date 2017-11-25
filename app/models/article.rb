50db0952980b1421849795ea8a18335d054877c3
Sergiiss <Dracula23@mail.ru>
2017-11-25 15:02:10 +0300

    Add image for article in main page

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,18 +6,12 @@ class Article < ApplicationRecord
     url: "/system/:hash.:extension", hash_secret: "longSecretsString"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-
-  before_save :edit_for_utc
-
   friendly_id :slug, use: :slugged
 
   validates :title, :description, :slug, :menu_label, :h1, :content, :published_at, presence: true
+  validates_numericality_of :priority, only_integer: true
 
   def normalize_friendly_id(input)
     input.to_s.to_slug.normalize(transliterations: :russian).to_s
-  end
-
-  def edit_for_utc
-    self.published_at -= 10800
   end
 end
