803d356db0df38c28ed88dd7d0e3caccc31024a5
Sergiiss <Dracula23@mail.ru>
2017-12-16 21:28:19 +0300

    Add tests for controller

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -12,4 +12,27 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
     assert_select "title", "Cms"
   end
+
+  test "should get show" do
+    article = Article.first
+
+    get article_path(article)
+
+    if article.nil?
+      assert_response :missing
+    else
+      assert_response :success
+
+      assert_select "div", 7
+    end
+  end
+
+
+  test "should get new" do
+    get new_article_path
+
+    assert_response :success
+
+    assert_select "form input", 9
+  end
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
   test "valid?" do
     article = Article.new(title: "Foo", h1: "Foo", description: "Foo", slug: "foo", menu_label: "foo", published_at: "2017-12-03 10:10:00", content: "<h1>Foo</h1>", priority: 2)
     # raise article.errors.full_messages.inspect
