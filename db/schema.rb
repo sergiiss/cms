1343f5bca0c17919da90e8ff9343b93136e35520
Sergiiss <Dracula23@mail.ru>
2017-12-25 23:31:23 +0300

    Add model User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171216203402) do
+ActiveRecord::Schema.define(version: 20171225202702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 20171216203402) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.index ["title"], name: "index_articles_on_title", unique: true, using: :btree
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
